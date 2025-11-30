efa08e9afed5e976cc315f8119466a287f31a075
heloise sá <heloisebsa.dev@gmail.com>
2025-11-30 14:09:09 -0300

    adiciona música as telas e cria tema
    versão estável aqui com mais 3 faixas de música pras telas de coleta, alto mar e end game. também adiciona um tema geral pra customizar a GUI.

--- Assets/Scripts/menu.gd
+++ Assets/Scripts/menu.gd
@@ -12,8 +12,8 @@ func _on_sair_pressed() -> void:
 
 
 func _on_créditos_pressed() -> void:
-	get_tree().change_scene_to_file("res://Assets/Scenes/creditos_scene.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scenes/Game Scenes/creditos_scene.tscn")
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Assets/Scenes/menu_scene.tscn")
+	get_tree().change_scene_to_file("res://Assets/Scenes/Game Scenes/menu_scene.tscn")
