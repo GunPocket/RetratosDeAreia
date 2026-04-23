51d3ded0aaa16ddd6bc88cfcf113e78bc15b2086
Haro Ukino <haroukino@hotmail.com>
2026-04-22 21:06:46 -0300

    REVERTED: hell

--- Assets/Scripts/camera.gd
+++ Assets/Scripts/camera.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var foto_barra: Node2D = $"../CanvasLayer/FotoBarra"
 
 @export var max_fotos: int = 4
+@export var textureSizeMultiplier: float
 var fotos_tiradas: int = 0
 
 const FOTO_SCENE = preload("res://Assets/Scenes/photo.tscn")
@@ -29,7 +30,7 @@ func _atualizar_label(qtd: int) -> void:
 	label.text = "Fotos tiradas: %d/%d" % [qtd, max_fotos]
 
 func _mostrar_foto() -> void:
-	var tex = _capturar_imagem()
+	var tex: = await _capturar_imagem()
 	var foto_instance = _criar_foto_instance(tex)
 
 	var resultados = _detectar_objetos_na_area()
@@ -50,14 +51,23 @@ func _capturar_imagem() -> ImageTexture:
 			var shape = foto.get_node_or_null("CollisionShape2D")
 			if shape:
 				shape.disabled = true
+	visible = false
+	await RenderingServer.frame_post_draw
 
 	var img: Image = get_viewport().get_texture().get_image()
-	var rect_size: Vector2 = collision_shape_2d.shape.extents * 2.0
-	var rect_pos: Vector2 = collision_shape_2d.global_position - rect_size / 2.0
-	var top_left: Vector2 = get_viewport().get_canvas_transform() * rect_pos
-	var rect: Rect2 = Rect2(top_left, rect_size)
+	var imgScale = Vector2( img.get_size() )/get_viewport_rect().size
+	var screen_pos: Vector2i = collision_shape_2d.get_global_transform_with_canvas().origin
+	var normalized_pos = Vector2( screen_pos ) / get_viewport_rect().size
+	var rect_size: Vector2 = (collision_shape_2d.shape.extents * textureSizeMultiplier) * imgScale
+	var rect_pos: Vector2 = (Vector2( img.get_size() ) * normalized_pos) - (rect_size/2)
+	var rect: Rect2 = Rect2(rect_pos, rect_size)
+	if (rect.position.x + rect.size.x) > img.get_size().x: rect.position.x -= (rect.position.x + rect.size.x) - img.get_size().x
+	if rect.position.x < 0: rect.position.x = 0
+	if (rect.position.y + rect.size.y) > img.get_size().y: rect.position.y -= (rect.position.y + rect.size.y) - img.get_size().y
+	if rect.position.y < 0: rect.position.y = 0
 	var recorte: Image = img.get_region(rect)
 	var tex = ImageTexture.create_from_image(recorte)
+	visible = true
 
 	for foto in fotos:
 		if foto != self:
